31b6ef4afbcc6eaf6a0717611f5e6afa61d57453
mbene dieye <mbenedieye757@gmail.com>
2025-06-23 16:14:08 +0200

    modified

--- lesson-1/src/App.jsx
+++ lesson-1/src/App.jsx
@@ -1,19 +1,49 @@
-const  App = () => {
- 
+import { useState } from "react"
+import Title from "./components/Title"
+
+const App = () => {
+
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+
+  const clearForm = () => {
+    setForm({
+      email: "",
+      password: ""
+    })
+  }
+
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+    setForm((form) => ({
+        ...form,
+        [name]: value,
+      }))
+  }
+
+   const handleSubmit = (event) =>{
+    event.preventdefault();
+    console.log(form)
+    clearForm();
+   }
 
   return (
     <>
-     
-     <form >
-      <div>
-        <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" placeholder="inserisci tuo email" />
-      </div>
-      <div>
-        <label htmlFor="password">Email</label>
-      <input type="password" id="password" name="password" placeholder="inserisci tuo password" />
-      </div>
-     </form>
+   
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input onInput={handleInput} value={form.email} type="email" id="email" name="email" placeholder="inserisci tuo email" />
+        </div>
+        <div>
+          <label htmlFor="password">Email</label>
+          <input onInput={handleInput} value={form.password} type="password" id="password" name="password" placeholder="inserisci tuo password" />
+        </div>
+        <button type="submit">Resistrati</button>
+      </form>
+         <Title email ={form.email} password ={form.password}/>
     </>
   )
 }
---
+++ lesson-1/src/components/Title.jsx
@@ -0,0 +1,10 @@
+const Title = ({email,password}) => {
+
+    return(
+        <>
+        <p>{email}</p>
+        <p>{password}</p>
+        </>
+    )
+}
+export default Title
